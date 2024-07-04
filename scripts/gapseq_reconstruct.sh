abcd6f6cb793686027d6052e5e5d27987cbe0227
Michael Predl <michael.predl@univie.ac.at>
2024-07-04 13:57:22 +0200

    Add a description header to the model reconstruction script

--- scripts/gapseq_reconstruct.sh
+++ scripts/gapseq_reconstruct.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Metabolic model reconstruction script for the cross-feeding-prediction protocol. More information here:
+# https://github.com/mpredl/cross-feeding-prediction-protocol
+# Reconstructs metabolic models based on input genomes using gapseq.
+
 # Variables
 TARGETDIR="/path/to/ouput_dir"  # Where to place the output files
 ENVDIR="/path/to/conda_env"  # The path to the conda environment
